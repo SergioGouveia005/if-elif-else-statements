e4f577225806d3bb947a4047ce0bcf481484c6f8
SergioGouveia005 <s5702924@bournemouth.ac.uk>
2024-10-22 19:05:23 +0100

    Finished fully functional Tower of Hanoi solver

--- tower_of_hanoi.py
+++ tower_of_hanoi.py
@@ -1,71 +1,65 @@
-def check_if_complete(peg_c, number_of_disks):
-    if len(peg_c) == 0:
-        return True
+import math
+def is_complete(peg_c, number_of_disks):
+    if len(peg_c) != number_of_disks:
+        return False
     for i in range(0,number_of_disks):
         if peg_c[i] != number_of_disks - i:
             return False
+    return True
 
 def solve_tower_of_hanoi(number_of_disks):
-    #if even smallest goes right
-    #if odd smallest goes left, wraps around
-    print(peg_a)
-    while(check_if_complete):
-        disk = peg_a.pop()
-        peg_b.append(disk)
-        if check_if_complete(peg_c, number_of_disks):
-            return
-        disk = peg_a.pop()
-        peg_c.append(disk)
-        if check_if_complete(peg_c, number_of_disks):
-            return
-        disk = peg_b.pop()
-        peg_c.append(disk)
-        if check_if_complete(peg_c, number_of_disks):
-            return
-        disk = peg_a.pop()
-        peg_b.append(disk)
-        if check_if_complete(peg_c, number_of_disks):
-            return
-        disk = peg_c.pop()
-        peg_a.append(disk)
-        if check_if_complete(peg_c, number_of_disks):
-            return
-        disk = peg_c.pop()
-        peg_b.append(disk)
-        if check_if_complete(peg_c, number_of_disks):
-            return
-        solve_tower_of_hanoi(number_of_disks)
-    else:
-        disk = peg_a.pop()
+    complete = False
+    move_smallest_piece = True
+    moves = 0
+    while(complete == False):
+        smallest_value = math.inf
+        movable_piece_index = 0
+        if move_smallest_piece:
+            for i in range(len(peg_list)):
+                if len(peg_list[i]) != 0 and peg_list[i][-1] == 1:
+                    moves += 1
+                    move_smallest_piece = False
+                    print(f"{moves}.",peg_list, end=" => ") #Display before move
+                    if number_of_disks % 2 == 0: peg_list[(i+1)%3].append(peg_list[i].pop())
+                    else:  peg_list[(i-1)%3].append(peg_list[i].pop())
+                    print(peg_list) #Display after move
+                    if is_complete(peg_c, number_of_disks): return
+                    break
+        else:
+            for i in range(len(peg_list)):
+                if len(peg_list[i]) != 0 and peg_list[i][-1] != 1:
+                    if peg_list[i][-1] < smallest_value:
+                        smallest_value = peg_list[i][-1]
+                        movable_piece_index = i
+            for i in range(len(peg_list)):
+                if len(peg_list[i]) == 0:
+                    moves += 1
+                    move_smallest_piece = True
+                    print(f"{moves}.",peg_list, end=" => ") #Display before move
+                    peg_list[i].append(peg_list[movable_piece_index].pop())
+                    print(peg_list) #Display after move
+                    if is_complete(peg_c, number_of_disks): return
+                    break
+                elif len(peg_list[i]) != 0 and peg_list[i][-1] > peg_list[movable_piece_index][-1]:
+                    moves += 1
+                    move_smallest_piece = True
+                    print(f"{moves}.",peg_list, end=" => ") #Display before move
+                    peg_list[i].append(peg_list[movable_piece_index].pop())
+                    print(peg_list) #Display after move
+                    if is_complete(peg_c, number_of_disks): return
+                    break
+
     
-    print(peg_a)
-    print(disk)
     return
 
-number_of_disks = 4
-moves = 0
+#Setting up the pegs
+number_of_disks = int(input("Enter the number of disks: "))
 peg_a = []
 peg_b = []
 peg_c = []
+peg_list = [peg_a,peg_b,peg_c]
 for i in range(0,number_of_disks):
     peg_a.append(number_of_disks - i)
+
+#Solving it
 solve_tower_of_hanoi(number_of_disks)
-
-# when even
-# a - b #
-# a - c
-# b - c #
-# a - b
-# c - a #
-# c - b
-# loop
-
-
-# when odd
-# a - c #
-# a - b
-# c - b #
-# a - c
-# b - a #
-# b - c
-# loop
